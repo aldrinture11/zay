9d73b2bbdbb7a1bf27eb4afe74f8ff778ed2c77b
aldrinture11 <152618241+aldrinture11@users.noreply.github.com>
2023-12-01 21:14:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
-const gif = document.querySelector(".zaya");
+const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
